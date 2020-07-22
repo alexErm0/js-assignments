0933589869b4ad29a35abca826eb461c0ba68479
AlexErm0 <ermolaevaleks26@gmail.com>
2020-07-22 15:03:15 +0300

    fixed indentations

--- task/06-conditions-n-loops-tasks.js
+++ task/06-conditions-n-loops-tasks.js
@@ -1,11 +1,11 @@
 'use strict';
 
 /**************************************************************************************************
- * *
- * Plese read the following tutorial before implementing tasks: *
- * https://developer.mozilla.org/en-US/docs/Web/JavaScript/Guide/Control_flow_and_error_handling *
- * https://developer.mozilla.org/en-US/docs/Web/JavaScript/Guide/Loops_and_iteration *
- * *
+ *                                                                                                *
+ * Plese read the following tutorial before implementing tasks:                                   *
+ * https://developer.mozilla.org/en-US/docs/Web/JavaScript/Guide/Control_flow_and_error_handling  *
+ * https://developer.mozilla.org/en-US/docs/Web/JavaScript/Guide/Loops_and_iteration              *
+ *                                                                                                *
  **************************************************************************************************/
 
 
@@ -20,13 +20,13 @@
  * @return {any}
  *
  * @example
- * 2 => 2
- * 3 => 'Fizz'
- * 5 => 'Buzz'
- * 4 => 4
- * 15 => 'FizzBuzz'
- * 20 => 'Buzz'
- * 21 => 'Fizz'
+ *   2 =>  2
+ *   3 => 'Fizz'
+ *   5 => 'Buzz'
+ *   4 => 4
+ *  15 => 'FizzBuzz'
+ *  20 => 'Buzz'
+ *  21 => 'Fizz'
  *
  */
 function getFizzBuzz(num) {
@@ -44,9 +44,9 @@ function getFizzBuzz(num) {
  * @return {number}
  *
  * @example:
- * 1 => 1
- * 5 => 120
- * 10 => 3628800
+ *   1  => 1
+ *   5  => 120
+ *   10 => 3628800
  */
 function getFactorial(n) {
 	let i=1;
@@ -67,9 +67,9 @@ function getFactorial(n) {
  * @return {number}
  *
  * @example:
- * 1,2 => 3 ( = 1+2 )
- * 5,10 => 45 ( = 5+6+7+8+9+10 )
- * -1,1 => 0 ( = -1 + 0 + 1 )
+ *   1,2   =>  3  ( = 1+2 )
+ *   5,10  =>  45 ( = 5+6+7+8+9+10 )
+ *   -1,1  =>  0  ( = -1 + 0 + 1 )
  */
 function getSumBetweenNumbers(n1, n2) {
 	let sum = 0;
@@ -87,10 +87,10 @@ function getSumBetweenNumbers(n1, n2) {
  * @return {bool}
  *
  * @example:
- * 1,2,3 => false
- * 3,4,5 => true
- * 10,1,1 => false
- * 10,10,10 => true
+ *   1,2,3    =>  false
+ *   3,4,5    =>  true
+ *   10,1,1   =>  false
+ *   10,10,10 =>  true
  */
 function isTriangle(a,b,c) {
 	if(a+b<=c)return false
@@ -103,19 +103,19 @@ function isTriangle(a,b,c) {
 /**
  * Returns true, if two specified axis-aligned rectangles overlap, otherwise false.
  * Each rectangle representing by object 
- * {
- * top: 5,
- * left: 5,
- * width: 20,
- * height: 10
- * }
+ *  {
+ *     top: 5,
+ *     left: 5,
+ *     width: 20,
+ *     height: 10
+ *  }
  * 
- * (5;5)
- * ------------- 
- * | | 
- * | | height = 10
- * ------------- 
- * width=20 
+ *  (5;5)
+ *     -------------  
+ *     |           | 
+ *     |           |  height = 10
+ *     ------------- 
+ *        width=20    
  * 
  * NOTE: Please use canvas coordinate space (https://developer.mozilla.org/en-US/docs/Web/API/Canvas_API/Tutorial/Drawing_shapes#The_grid),
  * it differs from Cartesian coordinate system.
@@ -125,12 +125,12 @@ function isTriangle(a,b,c) {
  * @return {bool}
  *
  * @example:
- * { top: 0, left: 0, width: 10, height: 10 },
- * { top: 5, left: 5, width: 20, height: 20 } => true
+ *   { top: 0, left: 0, width: 10, height: 10 },
+ *   { top: 5, left: 5, width: 20, height: 20 }    =>  true
  * 
- * { top: 0, left: 0, width: 10, height: 10 },
- * { top:20, left:20, width: 20, height: 20 } => false
- * 
+ *   { top: 0, left: 0, width: 10, height: 10 },
+ *   { top:20, left:20, width: 20, height: 20 }    =>  false
+ *  
  */
 function doRectanglesOverlap(rect1, rect2) {
 	function compare(rec1, rec2){
@@ -148,28 +148,28 @@ function doRectanglesOverlap(rect1, rect2) {
 /**
  * Returns true, if point lies inside the circle, otherwise false.
  * Circle is an object of 
- * {
- * center: {
- * x: 5, 
- * y: 5
- * }, 
- * radius: 20
- * }
+ *  {
+ *     center: {
+ *       x: 5,       
+ *       y: 5
+ *     },        
+ *     radius: 20
+ *  }
  * 
  * Point is object of 
- * {
- * x: 5,
- * y: 5
- * }
+ *  {
+ *     x: 5,
+ *     y: 5
+ *  }
  * 
  * @param {object} circle
  * @param {object} point
  * @return {bool}
  *
  * @example:
- * { center: { x:0, y:0 }, radius:10 }, { x:0, y:0 } => true
- * { center: { x:0, y:0 }, radius:10 }, { x:10, y:10 } => false
- * 
+ *   { center: { x:0, y:0 }, radius:10 },  { x:0, y:0 }     => true
+ *   { center: { x:0, y:0 }, radius:10 },  { x:10, y:10 }   => false
+ *   
  */
 function isInsideCircle(circle, point) {
 	let dirLine= Math.hypot(circle.center.x - point.x ,circle.center.y - point.y);
@@ -184,9 +184,9 @@ function isInsideCircle(circle, point) {
  * @return {string}
  *
  * @example:
- * 'The quick brown fox jumps over the lazy dog' => 'T'
- * 'abracadabra' => 'c'
- * 'entente' => null
+ *   'The quick brown fox jumps over the lazy dog' => 'T'
+ *   'abracadabra'  => 'c'
+ *   'entente' => null
  */
 function findFirstSingleChar(str) {
 	for(let i =0;i<str.length;i++){
@@ -213,12 +213,12 @@ function findFirstSingleChar(str) {
  * @return {string}
  *
  * @example
- * 0, 1, true, true => '[0, 1]'
- * 0, 1, true, false => '[0, 1)'
- * 0, 1, false, true => '(0, 1]'
- * 0, 1, false, false => '(0, 1)'
+ *   0, 1, true, true   => '[0, 1]'
+ *   0, 1, true, false  => '[0, 1)'
+ *   0, 1, false, true  => '(0, 1]'
+ *   0, 1, false, false => '(0, 1)'
  * Smaller number has to be first :
- * 5, 3, true, true => '[3, 5]'
+ *   5, 3, true, true   => '[3, 5]'
  *
  */
 function getIntervalString(a, b, isStartIncluded, isEndIncluded) {
@@ -258,10 +258,10 @@ function reverseString(str) {
  * @return {number}
  *
  * @example:
- * 12345 => 54321
- * 1111 => 1111
- * 87354 => 45378
- * 34143 => 34143
+ *   12345 => 54321
+ *   1111  => 1111
+ *   87354 => 45378
+ *   34143 => 34143
  */
 function reverseInteger(num) {
 	let str = 0;
@@ -281,15 +281,15 @@ function reverseInteger(num) {
  * @return {boolean}
  *
  * @example:
- * 79927398713 => true
- * 4012888888881881 => true
- * 5123456789012346 => true
- * 378282246310005 => true
- * 371449635398431 => true
+ *   79927398713      => true
+ *   4012888888881881 => true
+ *   5123456789012346 => true
+ *   378282246310005  => true
+ *   371449635398431  => true
  *
- * 4571234567890111 => false
- * 5436468789016589 => false
- * 4916123456789012 => false
+ *   4571234567890111 => false
+ *   5436468789016589 => false
+ *   4916123456789012 => false
  */
 function isCreditCardNumber(ccn) {
 	let sum = 0;
@@ -310,17 +310,17 @@ function isCreditCardNumber(ccn) {
 
 /**
  * Returns the digital root of integer:
- * step1 : find sum of all digits
- * step2 : if sum > 9 then goto step1 otherwise return the sum
+ *   step1 : find sum of all digits
+ *   step2 : if sum > 9 then goto step1 otherwise return the sum
  *
  * @param {number} n
  * @return {number}
  *
  * @example:
- * 12345 ( 1+2+3+4+5 = 15, 1+5 = 6) => 6
- * 23456 ( 2+3+4+5+6 = 20, 2+0 = 2) => 2
- * 10000 ( 1+0+0+0+0 = 1 ) => 1
- * 165536 (1+6+5+5+3+6 = 26, 2+6 = 8) => 8
+ *   12345 ( 1+2+3+4+5 = 15, 1+5 = 6) => 6
+ *   23456 ( 2+3+4+5+6 = 20, 2+0 = 2) => 2
+ *   10000 ( 1+0+0+0+0 = 1 ) => 1
+ *   165536 (1+6+5+5+3+6 = 26,  2+6 = 8) => 8
  */
 function getDigitalRoot(num) {
 	let sum = num;
@@ -345,16 +345,16 @@ function getDigitalRoot(num) {
  * @return {boolean}
  *
  * @example:
- * '' => true
- * '[]' => true
- * '{}' => true
- * '() => true
- * '[[]' => false
- * '][' => false
- * '[[][][[]]]' => true
- * '[[][]][' => false
- * '{)' = false
- * '{[(<{[]}>)]}' = true 
+ *   '' => true
+ *   '[]'  => true
+ *   '{}'  => true
+ *   '()   => true
+ *   '[[]' => false
+ *   ']['  => false
+ *   '[[][][[]]]' => true
+ *   '[[][]][' => false
+ *   '{)' = false
+ *   '{[(<{[]}>)]}' = true 
  */
 function isBracketsBalanced(str) {
 	let massIn = ["{", "[", "(", "<"];
@@ -403,19 +403,19 @@ function isBracketsBalanced(str) {
  * The result string should be constrcuted using the folliwing rules:
  *
  * ---------------------------------------------------------------------
- * Difference | Result
+ *   Difference                 |  Result
  * ---------------------------------------------------------------------
- * 0 to 45 seconds | a few seconds ago
- * 45 to 90 seconds | a minute ago
- * 90 seconds to 45 minutes | 2 minutes ago ... 45 minutes ago
- * 45 to 90 minutes | an hour ago
- * 90 minutes to 22 hours | 2 hours ago ... 22 hours ago
- * 22 to 36 hours | a day ago
- * 36 hours to 25 days | 2 days ago ... 25 days ago
- * 25 to 45 days | a month ago
- * 45 to 345 days | 2 months ago ... 11 months ago
- * 345 to 545 days (1.5 years) | a year ago
- * 546 days+ | 2 years ago ... 20 years ago
+ *    0 to 45 seconds           |  a few seconds ago
+ *   45 to 90 seconds           |  a minute ago
+ *   90 seconds to 45 minutes   |  2 minutes ago ... 45 minutes ago
+ *   45 to 90 minutes           |  an hour ago
+ *  90 minutes to 22 hours      |  2 hours ago ... 22 hours ago
+ *  22 to 36 hours              |  a day ago
+ *  36 hours to 25 days         |  2 days ago ... 25 days ago
+ *  25 to 45 days               |  a month ago
+ *  45 to 345 days              |  2 months ago ... 11 months ago
+ *  345 to 545 days (1.5 years) |  a year ago
+ *  546 days+                   |  2 years ago ... 20 years ago
  * ---------------------------------------------------------------------
  *
  * @param {Date} startDate
@@ -423,10 +423,10 @@ function isBracketsBalanced(str) {
  * @return {string}
  *
  * @example
- * Date('2000-01-01 01:00:00.100'), Date('2000-01-01 01:00:00.200') => 'a few seconds ago'
- * Date('2000-01-01 01:00:00.100'), Date('2000-01-01 01:00:05.000') => '5 minutes ago'
- * Date('2000-01-01 01:00:00.100'), Date('2000-01-02 03:00:05.000') => 'a day ago'
- * Date('2000-01-01 01:00:00.100'), Date('2015-01-02 03:00:05.000') => '15 years ago'
+ *   Date('2000-01-01 01:00:00.100'), Date('2000-01-01 01:00:00.200')  => 'a few seconds ago'
+ *   Date('2000-01-01 01:00:00.100'), Date('2000-01-01 01:00:05.000')  => '5 minutes ago'
+ *   Date('2000-01-01 01:00:00.100'), Date('2000-01-02 03:00:05.000')  => 'a day ago'
+ *   Date('2000-01-01 01:00:00.100'), Date('2015-01-02 03:00:05.000')  => '15 years ago'
  *
  */
 function timespanToHumanString(startDate, endDate) {
@@ -457,12 +457,12 @@ function timespanToHumanString(startDate, endDate) {
  * @return {string}
  *
  * @example:
- * 1024, 2 => '10000000000'
- * 6561, 3 => '100000000'
- * 365, 2 => '101101101'
- * 365, 3 => '111112'
- * 365, 4 => '11231'
- * 365, 10 => '365'
+ *   1024, 2  => '10000000000'
+ *   6561, 3  => '100000000'
+ *    365, 2  => '101101101'
+ *    365, 3  => '111112'
+ *    365, 4  => '11231'
+ *    365, 10 => '365'
  */
 function toNaryString(num, n) {
 	let str = "";
@@ -496,10 +496,10 @@ function toNaryString(num, n) {
  * @return {string}
  *
  * @example:
- * ['/web/images/image1.png', '/web/images/image2.png'] => '/web/images/'
- * ['/web/assets/style.css', '/web/scripts/app.js', 'home/setting.conf'] => ''
- * ['/web/assets/style.css', '/.bin/mocha', '/read.me'] => '/'
- * ['/web/favicon.ico', '/web-scripts/dump', '/webalizer/logs'] => '/'
+ *   ['/web/images/image1.png', '/web/images/image2.png']  => '/web/images/'
+ *   ['/web/assets/style.css', '/web/scripts/app.js',  'home/setting.conf'] => ''
+ *   ['/web/assets/style.css', '/.bin/mocha',  '/read.me'] => '/'
+ *   ['/web/favicon.ico', '/web-scripts/dump', '/webalizer/logs'] => '/'
  */
 function getCommonDirectoryPath(pathes) {
 	let Piece = "";
@@ -529,13 +529,13 @@ function getCommonDirectoryPath(pathes) {
  * @return {array}
  *
  * @example:
- * [[ 1, 0, 0 ], [[ 1, 2, 3 ], [[ 1, 2, 3 ],
- * [ 0, 1, 0 ], X [ 4, 5, 6 ], => [ 4, 5, 6 ],
- * [ 0, 0, 1 ]] [ 7, 8, 9 ]] [ 7, 8, 9 ]]
+ *   [[ 1, 0, 0 ],       [[ 1, 2, 3 ],           [[ 1, 2, 3 ],
+ *    [ 0, 1, 0 ],   X    [ 4, 5, 6 ],     =>     [ 4, 5, 6 ],
+ *    [ 0, 0, 1 ]]        [ 7, 8, 9 ]]            [ 7, 8, 9 ]]
  *
- * [[ 4 ],
- * [[ 1, 2, 3]] X [ 5 ], => [[ 32 ]]
- * [ 6 ]]
+ *                        [[ 4 ],
+ *   [[ 1, 2, 3]]    X     [ 5 ],          =>     [[ 32 ]]
+ *                         [ 6 ]]
  *
  */
 function getMatrixProduct(m1, m2) {
@@ -569,21 +569,21 @@ function getMatrixProduct(m1, m2) {
  *
  * @example
  *
- * [[ 'X', ,'0' ],
- * [ ,'X','0' ], => 'X'
- * [ , ,'X' ]]
+ *   [[ 'X',   ,'0' ],
+ *    [    ,'X','0' ],       =>  'X'
+ *    [    ,   ,'X' ]]
  *
- * [[ '0','0','0' ],
- * [ ,'X', ], => '0'
- * [ 'X', ,'X' ]]
+ *   [[ '0','0','0' ],
+ *    [    ,'X',    ],       =>  '0'
+ *    [ 'X',   ,'X' ]]
  *
- * [[ '0','X','0' ],
- * [ ,'X', ], => undefined
- * [ 'X','0','X' ]]
+ *   [[ '0','X','0' ],
+ *    [    ,'X',    ],       =>  undefined
+ *    [ 'X','0','X' ]]
  *
- * [[ , , ],
- * [ , , ], => undefined
- * [ , , ]]
+ *   [[    ,   ,    ],
+ *    [    ,   ,    ],       =>  undefined
+ *    [    ,   ,    ]]
  *
  */
 function evaluateTicTacToePosition(position) {
@@ -615,22 +615,22 @@ function evaluateTicTacToePosition(position) {
 
 
 module.exports = {
- getFizzBuzz: getFizzBuzz,
- getFactorial: getFactorial,
- getSumBetweenNumbers: getSumBetweenNumbers,
- isTriangle: isTriangle,
- doRectanglesOverlap: doRectanglesOverlap,
- isInsideCircle: isInsideCircle,
- findFirstSingleChar: findFirstSingleChar,
- getIntervalString : getIntervalString,
- reverseString: reverseString,
- reverseInteger: reverseInteger,
- isCreditCardNumber: isCreditCardNumber,
- getDigitalRoot: getDigitalRoot,
- isBracketsBalanced: isBracketsBalanced,
- timespanToHumanString : timespanToHumanString,
- toNaryString: toNaryString,
- getCommonDirectoryPath: getCommonDirectoryPath,
- getMatrixProduct: getMatrixProduct,
- evaluateTicTacToePosition : evaluateTicTacToePosition
+    getFizzBuzz: getFizzBuzz,
+    getFactorial: getFactorial,
+    getSumBetweenNumbers: getSumBetweenNumbers,
+    isTriangle: isTriangle,
+    doRectanglesOverlap: doRectanglesOverlap,
+    isInsideCircle: isInsideCircle,
+    findFirstSingleChar: findFirstSingleChar,
+    getIntervalString : getIntervalString,
+    reverseString: reverseString,
+    reverseInteger: reverseInteger,
+    isCreditCardNumber: isCreditCardNumber,
+    getDigitalRoot: getDigitalRoot,
+    isBracketsBalanced: isBracketsBalanced,
+    timespanToHumanString : timespanToHumanString,
+    toNaryString: toNaryString,
+    getCommonDirectoryPath: getCommonDirectoryPath,
+    getMatrixProduct: getMatrixProduct,
+    evaluateTicTacToePosition : evaluateTicTacToePosition
 };
